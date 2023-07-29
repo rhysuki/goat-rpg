97b6b8eb1bcaeaaf3738ca001d42ee0fe04d4401
rhy <81539300+rhynomatt@users.noreply.github.com>
2023-07-29 19:58:46 -0300

    Update test maps

--- data/tiled/tilesets/test.tsx
+++ data/tiled/tilesets/test.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="16" tileheight="16" tilecount="4" columns="4">
- <image source="test.png" width="64" height="16"/>
+<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="test.png" width="80" height="16"/>
  <tile id="1">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
