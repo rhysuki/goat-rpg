422f25ea6c7ede37252e98041e34a16b3991a9ad
rhy <81539300+rhynomatt@users.noreply.github.com>
2023-07-29 15:41:54 -0300

    Add a test map

---
+++ data/tiled/tilesets/test.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="test.png" width="64" height="16"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
