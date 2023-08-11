26db567dc9622db03d874afd628c5ddd1c336607
rhy <81539300+rhynomatt@users.noreply.github.com>
2023-08-11 00:24:21 -0300

    Extrude the tile sprites in the test tileset
    Seems this fixes the sometimes-uneven quads? We'll see

--- data/tiled/tilesets/test.tsx
+++ data/tiled/tilesets/test.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="16" tileheight="16" tilecount="5" columns="5">
- <image source="test.png" width="80" height="16"/>
+<tileset version="1.5" tiledversion="1.7.2" name="test" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="10" columns="10">
+ <image source="../tilesets/test.png" width="180" height="18"/>
  <tile id="1">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
@@ -8,12 +8,42 @@
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="3">
-  <objectgroup draworder="index" id="3">
-   <object id="2" x="0" y="0" width="16" height="16"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
 </tileset>
